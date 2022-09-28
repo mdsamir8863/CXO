1f145d9a685a6f954da23fcc54e900779478822d
Mentoons Developer <mentoonsdeveloper.ansari@gmail.com>
2022-09-28 15:37:10 +0530

    update

--- index.js
+++ index.js
@@ -18,13 +18,17 @@ hide.style.display = "none";
 function basicFunc() {
   hide.style.display = "none";
   price.innerText = "Price just 15,999 Rupees";
-  basic.style.backgroundColor = "gray";
-  premiumbtn.style.backgroundColor = "aquamarine";
+  basic.style.backgroundColor = "aquamarine";
+  // premiumbtn.style.backgroundColor = "rgb(127, 165, 255)";
+  premiumbtn.style.backgroundColor = "white";
+
 }
 
 function premium() {
   price.innerText = "Price just 19,999 Rupees";
-  basic.style.backgroundColor = "aquamarine";
-  premiumbtn.style.backgroundColor = "gray";
+  // basic.style.backgroundColor = "rgb(127, 165, 255)";
+  basic.style.backgroundColor = "white";
   hide.style.display = "block";
+  premiumbtn.style.backgroundColor = "aquamarine";
+
 }
