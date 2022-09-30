5e122c0356ad83ee50b6bf82c6179fe2bc3dc28b
Mentoons Developer <mentoonsdeveloper.ansari@gmail.com>
2022-09-30 16:49:37 +0530

    updating

--- index.js
+++ index.js
@@ -69,38 +69,60 @@ topArrow.addEventListener("click", () => {
 });
 
 // CAROUSEL JS START
-let pagination = document.querySelectorAll(".ball");
-let carousel = document.getElementById("carousel");
-let cards = document.querySelectorAll(".card");
-let leftBtn = document.getElementById("leftArrow");
-let rightBtn = document.getElementById("rightArrow");
-let section = 0;
+// SETUP
 
-const updatePag = () => {
-  pagination.forEach((el) => (el.style.width = "12px"));
-  pagination[section].style.width = "25px";
-};
+// Sets variables to return inner div and images
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselImages = document.querySelectorAll(".carousel-slide img");
 
-pagination.forEach((curItem, index) => {
-  curItem.addEventListener("click", () => {
-    section = index;
-    updatePag();
-    carousel.scrollLeft = index * (window.innerWidth - 150) + index * 10;
-  });
+// Sets variables to return buttons
+const prevBtn = document.querySelector("#prevbutton");
+const nextBtn = document.querySelector("#nextbutton");
+
+// Creates a counter to track which image we are on
+let counter = 1;
+// Selects initial width of image so prog knows how much to slide left or right
+const size = carouselImages[0].clientWidth;
+// Moves carousel back to start with first image instead of clone
+carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+
+// FUNCTIONS
+
+// Add a function to track next button clicks
+nextBtn.addEventListener("click", function () {
+  // If user reaches end of carousel, stop execution
+  if (counter >= carouselImages.length - 1) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  // Add 1 to the counter
+  counter++;
+  // Move carousel
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
 });
 
-leftBtn.addEventListener("click", () => {
-  if (section > 0) {
-    section--;
-    carousel.scrollLeft = section * (window.innerWidth - 150) + section * 10;
-    updatePag();
+//create function to look for PREV button clicks
+prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return;
+  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+});
+
+// This function is triggered after transformation
+carouselSlide.addEventListener("transitionend", () => {
+  if (carouselImages[counter].id === "lastClone") {
+    // Removes transition effect and translates back to original picture
+    carouselSlide.style.transition = "none";
+    // -2 because we have duplicate and first image
+    counter = carouselImages.length - 2;
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  }
+  if (carouselImages[counter].id === "firstClone") {
+    //removes transition effect and translates back to original picture
+    carouselSlide.style.transition = "none";
+    //-2 because we have duplicate and first
+    counter = carouselImages.length - counter;
+    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
   }
 });
-rightBtn.addEventListener("click", () => {
-  if (section < 2) {
-    section++;
-    carousel.scrollLeft = section * (window.innerWidth - 150) + section * 10;
-    updatePag();
-  }
-});
+
 // CAROUSEL JS END
