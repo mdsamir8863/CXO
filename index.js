2aea17aa52f3ca48c9833577eafe95dfaae63614
Mentoons Developer <mentoonsdeveloper.ansari@gmail.com>
2022-09-30 11:29:23 +0530

    updating

--- index.js
+++ index.js
@@ -67,3 +67,40 @@ topArrow.addEventListener("click", () => {
 
   console.log("hello");
 });
+
+// CAROUSEL JS START
+let pagination = document.querySelectorAll(".ball");
+let carousel = document.getElementById("carousel");
+let cards = document.querySelectorAll(".card");
+let leftBtn = document.getElementById("leftArrow");
+let rightBtn = document.getElementById("rightArrow");
+let section = 0;
+
+const updatePag = () => {
+  pagination.forEach((el) => (el.style.width = "12px"));
+  pagination[section].style.width = "25px";
+};
+
+pagination.forEach((curItem, index) => {
+  curItem.addEventListener("click", () => {
+    section = index;
+    updatePag();
+    carousel.scrollLeft = index * (window.innerWidth - 150) + index * 10;
+  });
+});
+
+leftBtn.addEventListener("click", () => {
+  if (section > 0) {
+    section--;
+    carousel.scrollLeft = section * (window.innerWidth - 150) + section * 10;
+    updatePag();
+  }
+});
+rightBtn.addEventListener("click", () => {
+  if (section < 2) {
+    section++;
+    carousel.scrollLeft = section * (window.innerWidth - 150) + section * 10;
+    updatePag();
+  }
+});
+// CAROUSEL JS END
